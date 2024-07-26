fecef89ed686c038d6270a050d8e35e9079d2f7b
valenrey11 <valen.9298@gmail.com>
2024-07-26 13:57:34 -0300

    feat: add button to get random address between bounds

---
+++ src/components/ButtonRandomAddress.jsx
@@ -0,0 +1,43 @@
+export function ButtonRandomAddress({ bounds, setBounds, setAddressToShow }) {
+  const doing = () => {
+    function getRandomCoordinate(bounds) {
+      console.log('los bounds', bounds)
+      const lat = Math.random() * (bounds?.[0][0] - bounds?.[1][0]) + bounds?.[1][0]
+      const lon = Math.random() * (bounds?.[0][1] - bounds?.[1][1]) + bounds?.[1][1]
+      return { lat, lon }
+    }
+    const { lat, lon } = getRandomCoordinate(bounds)
+    console.log(lat, lon)
+
+    fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&addressdetails=1`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data', data)
+        if (data && data.address) {
+          const { road, house_number, city, country } = data.address
+          if (road && house_number) {
+            const addressForUser = `${road} ${house_number}`
+            const address = `${house_number} ${road}, ${city}, ${country}`
+            setAddressToShow(addressForUser)
+          } else {
+            doing()
+            console.log('No se encontró ninguna dirección.')
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Error al obtener la dirección:', error)
+      })
+  }
+
+  return (
+    <button
+      className='px-5 py-3 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center  dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800'
+      type='button'
+      onClick={doing}>
+      Get random address
+    </button>
+  )
+}
